98cb7c8207cb43dab4eccb3d546aeecf108da313
Khaïs COLIN <khais.colin@gmail.com>
2022-08-20 14:15:59 +0200

    Correctly read unquotes

--- impls/rust-logistic-bot/src/reader.rs
+++ impls/rust-logistic-bot/src/reader.rs
@@ -81,6 +81,10 @@ fn read_form(reader: &mut Reader) -> Result<Atom, ParseError> {
                 Atom::Symbol(String::from("quasiquote")),
                 read_form(reader)?,
             ])),
+            "~" => Ok(Atom::List(vec![
+                Atom::Symbol(String::from("unquote")),
+                read_form(reader)?,
+            ])),
             _ => Ok(read_atom(token)),
         },
     }
